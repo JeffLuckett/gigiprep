c1be84e4f08e5548d47224670e380c8c7a73bb86
Jeff Luckett <jeff@jeffluckett.com>
2013-04-21 21:11:05 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gigiprep::Application.config.secret_token = 'a6fb045689a398ef3de77891a26c0064b37c45bc3c284022b48d1c554b521896d5ba3652b45930f3252760d033780a216f635b32017ddf4e1419362309b1e62e'
